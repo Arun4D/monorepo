53c74addd1b666fe45a4d5e6e219c6d0d2081bc8
Arun Duraisamy <arun4.duraisamy@gmail.com>
2022-08-23 00:25:44 +0530

    Added health check script

---
+++ ci/allContainerHealthCheck.sh
@@ -0,0 +1,13 @@
+container=$1;
+for cont in ${container[@]}
+do
+  ./ci/healthcheck.sh $cont;
+  
+  if [ $? -eq 0 ]
+  then
+    echo "$cont is running"
+  else
+    echo "$cont is not running. Please check the logs."
+    exit 1
+  fi 
+done
--- ci/healthcheck.sh
+++ ci/healthcheck.sh
@@ -10,15 +10,17 @@ fi
 
 echo "Container: $container_name";
 echo "Timeout: $timeout sec";
-initalStatus=$(docker inspect --format='{{json .State.Health}}' $container_name)
-echo "Inital Status: $initalStatus";
+
+#initalStatus=$(docker inspect --format='{{json .State}}' $container_name)
+#echo "Inital Status: $initalStatus";
+
 try=0;
 is_healthy="false";
 while [ $is_healthy != "true" ];
 do
   try=$(($try + 1));
   printf "■";
-  is_healthy=$(docker inspect --format='{{json .State.Health}}' $container_name | jq '.Status == "healthy"');
+  is_healthy=$(docker inspect --format='{{json .State}}' $container_name | jq '.Running == true');
   sleep 1;
   if [[ $try -eq $timeout ]]; then
     echo " Container did not boot within timeout";
